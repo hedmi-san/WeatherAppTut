8d9496915f932fb16fb95f0f2dcaf07a243c68bf
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-07-30 21:16:57 +0100

    completing the ui
    finishing the app

--- lib/features/Home/view/home.dart
+++ lib/features/Home/view/home.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
@@ -13,14 +14,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final WeatherFactory _wf = WeatherFactory(OPEN_WEATHER_API_KEY);
   Weather? _weather;
+  Timer? _timer;
+
   @override
   void initState() {
     super.initState();
+    _updateWeather();
+    _timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      setState(() {});
+    });
+  }
+
+  void _updateWeather() {
     _wf.currentWeatherByCityName('El Eulma').then((w) {
       setState(() {
         _weather = w;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
@@ -56,16 +72,24 @@ class _HomeState extends State<Home> {
           ),
           _dateTimeInfo(),
           SizedBox(
-            height: MediaQuery.sizeOf(context).height * 0.08,
+            height: MediaQuery.sizeOf(context).height * 0.05,
           ),
           _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _currentTemp(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _extraInfo()
         ],
       ),
     );
   }
 
   Widget _dateTimeInfo() {
-    DateTime now = _weather!.date!;
+    DateTime now = DateTime.now();
     return Column(
       children: [
         Text(
@@ -90,7 +114,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              '  ${DateFormat("d.m.y").format(now)}',
+              '  ${DateFormat("d.M.y").format(now)}',
               style: const TextStyle(
                 fontSize: 15,
                 fontWeight: FontWeight.w400,
@@ -109,15 +133,99 @@ class _HomeState extends State<Home> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
+          height: MediaQuery.sizeOf(context).height * 0.2,
           decoration: BoxDecoration(
             image: DecorationImage(
               image: NetworkImage(
-                _weather!.weatherIcon!,
+                "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
               ),
             ),
           ),
+        ),
+        Text(
+          _weather?.weatherDescription ?? '',
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
         )
       ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      '${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C',
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.15,
+      width: MediaQuery.sizeOf(context).width * 0.80,
+      decoration: BoxDecoration(
+        color: Colors.deepPurpleAccent,
+        borderRadius: BorderRadius.circular(
+          20,
+        ),
+      ),
+      padding: const EdgeInsets.all(
+        8.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Weather Demo',
       home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
